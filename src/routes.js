848874a7a7f38023eb99b2e5c80f5913ff004f61
Luiz1nn <luisfernandobr2003@gmail.com>
2025-06-16 11:26:17 -0300

    🗃️ database: implement json file database with persistence and data selection

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,15 @@
+import { Database } from './database.js'
+
+const database = new Database()
+
 export const routes = [
   {
     method: 'GET',
     path: '/tasks',
     handler: (req, res) => {
-      return res.end(JSON.stringify([]))
+      const users = database.select('users')
+
+      return res.end(JSON.stringify(users))
     }
   }
 ]
